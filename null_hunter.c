a7205a3d12992073fbd981a4b15531d5c08460eb
Bijal Thanawala <72908+bijalthanawala@users.noreply.github.com>
2025-10-28 15:00:39 +1100

    Count segments (single stretches) of NULL

--- null_hunter.c
+++ null_hunter.c
@@ -4,7 +4,8 @@ int main(int argc, char **argv) {
 
     FILE *fp = NULL;
     char ch = 0;
-    int count = 0;
+    int null_segments = 0;
+    int total_null_count = 0;
 
     if(argc < 2) {
         printf("Please specify the file to look NULL into!\n");
@@ -13,13 +14,19 @@ int main(int argc, char **argv) {
 
     printf("Scanning file %s\n", argv[1]);
     fp = fopen(argv[1], "rb");
-    while((ch = fgetc(fp)) != -1) {
-        if(ch == 0) {
-            count++;
+    while(ch != -1) {
+        ch = fgetc(fp);
+
+        if(ch == 0)
+            null_segments++;
+
+        while(ch == 0) {
+            total_null_count++;
+            ch = fgetc(fp);
         }
     }
 
-    printf("Found %d NULL characters\n", count);
+    printf("Found total %d NULL characters in %d segments\n", total_null_count, null_segments);
 
     return 0;
 }
