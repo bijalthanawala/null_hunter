328b6b2cd5f1275569e47718ad3da77f23cf14b4
Bijal Thanawala <72908+bijalthanawala@users.noreply.github.com>
2025-10-28 16:47:00 +1100

    Refactoring

--- null_hunter.c
+++ null_hunter.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 
+typedef struct null_stats {
+    int total_null_count;
+    int null_segments;
+} NULL_STATS, *P_NULL_STATS;
+
+void count_null(FILE* fp, P_NULL_STATS p_null_stats) {
+    char ch = 0;
+
+    p_null_stats->null_segments = 0;
+    p_null_stats->total_null_count = 0;
+
+    while(ch != -1) {
+        ch = fgetc(fp);
+
+        if(ch == 0)
+            p_null_stats->null_segments++;
+
+        while(ch == 0) {
+            p_null_stats->total_null_count++;
+            ch = fgetc(fp);
+        }
+    }
+}
+
 int main(int argc, char **argv) {
 
     FILE *fp = NULL;
-    char ch = 0;
-    int null_segments = 0;
-    int total_null_count = 0;
+    NULL_STATS null_stats = {0};
 
     if(argc < 2) {
         printf("Please specify the file to look NULL into!\n");
@@ -14,19 +36,12 @@ int main(int argc, char **argv) {
 
     printf("Scanning file %s\n", argv[1]);
     fp = fopen(argv[1], "rb");
-    while(ch != -1) {
-        ch = fgetc(fp);
+    count_null(fp, &null_stats);
 
-        if(ch == 0)
-            null_segments++;
-
-        while(ch == 0) {
-            total_null_count++;
-            ch = fgetc(fp);
-        }
-    }
-
-    printf("Found total %d NULL characters in %d segments\n", total_null_count, null_segments);
+    printf("Found total %d NULL characters in %d segments\n",
+        null_stats.total_null_count,
+        null_stats.null_segments
+    );
 
     return 0;
 }
